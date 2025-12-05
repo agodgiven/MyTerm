fb49377af240da7a1271984ded7c704bbe6de305
agodgiven <119949124+agodgiven@users.noreply.github.com>
2025-12-05 16:07:26 +0300

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
   output: 'export',
   distDir: '../public',
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Andrey Bogdanov',
+    description: 'Test',
     icon: '/img/logo.svg',
     listUrl: 'https://registry.kasmweb.com/',
     contactUrl: 'https://kasmweb.com/support',
